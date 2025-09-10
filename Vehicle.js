cf6aaec451151d20f4d88a97733a56a24c306984
Andreas Lagerström <andreas.lagerstrom@gmail.com>
2025-09-10 21:05:01 +0200

    one vehicle to rule them all

--- Vehicle.js
+++ Vehicle.js
@@ -17,6 +17,7 @@ class Vehicle extends GameObject {
         this.steeringChange = 5;
         this.rect = new Rectangle(0, 0, this.width, this.height, color);
         this.playerEntered = false;
+        this.easySteering = false;
         this.img = new Image();
         this.img.src = 'img/car-person.png';
     }
@@ -50,7 +51,135 @@ class Vehicle extends GameObject {
         if (this.speed < -this.maxReverseSpeed) this.speed = -this.maxReverseSpeed;
     }
 
-    update(deltaTime, keyboard) {
+    left(deltaTime) {
+        this.targetAngle = Math.PI
+        this.acc(deltaTime)
+    }
+
+    leftDown(deltaTime) {
+        this.targetAngle = Math.PI - Math.PI / 4
+        this.acc(deltaTime)
+    }
+
+    leftUp(deltaTime) {
+        this.targetAngle = Math.PI + Math.PI / 4
+        this.acc(deltaTime)
+    }
+
+    rightDown(deltaTime) {
+        this.targetAngle = Math.PI / 4
+        this.acc(deltaTime)
+    }
+
+    rightUp(deltaTime) {
+        this.targetAngle = 3 * Math.PI / 2 + Math.PI / 4
+        this.acc(deltaTime)
+    }
+
+    right(deltaTime) {
+        this.targetAngle = 0
+        this.acc(deltaTime)
+    }
+
+    up(deltaTime) {
+        this.targetAngle = 3 * Math.PI / 2
+        this.acc(deltaTime)
+    }
+
+    acc(deltaTime) {
+        if (this.speed < 0) {
+            this.speed += this.braking * deltaTime;
+        } else {
+            this.speed += this.acceleration * deltaTime;
+        }
+    }
+
+    dec(deltaTime) {
+        this.speed -= this.braking * deltaTime;
+        if (this.speed < 0) this.speed = 0;
+    }
+
+    down(deltaTime) {
+        this.targetAngle = Math.PI / 2
+        this.acc(deltaTime)
+    }
+
+    easySteer(deltaTime, keyboard) {
+        if (keyboard !== null && keyboard !== undefined) {
+            if (keyboard.isKeyDown(Buttons.LEFT) && keyboard.isKeyDown(Buttons.DOWN)) {
+                this.leftDown(deltaTime)
+            } else if (keyboard.isKeyDown(Buttons.LEFT) && keyboard.isKeyDown(Buttons.UP)) {
+                this.leftUp(deltaTime)
+            } else if (keyboard.isKeyDown(Buttons.RIGHT) && keyboard.isKeyDown(Buttons.DOWN)) {
+                this.rightDown(deltaTime)
+            } else if (keyboard.isKeyDown(Buttons.RIGHT) && keyboard.isKeyDown(Buttons.UP)) {
+                this.rightUp(deltaTime)
+            } else if (keyboard.isKeyDown(Buttons.LEFT)) {
+                this.left(deltaTime)
+            } else if (keyboard.isKeyDown(Buttons.RIGHT)) {
+                this.right(deltaTime)
+            } else if (keyboard.isKeyDown(Buttons.UP)) {
+                this.up(deltaTime)
+            } else if (keyboard.isKeyDown(Buttons.DOWN)) {
+                this.down(deltaTime)
+            } else {
+                this.dec(deltaTime)
+            }
+        }
+
+        const friction = 0.5;
+        this.speed *= Math.pow(friction, deltaTime);
+
+        console.log("angle: " + this.angle + ", target: " + this.targetAngle)
+
+        if (this.angle >= 2 * Math.PI) {
+            this.angle -= 2 * Math.PI;
+        }
+        if (this.angle < 0) {
+            this.angle += 2 * Math.PI;
+        }
+
+        let diff = this.angle - this.targetAngle;
+
+        let goTheOtherWay = false
+        if (Math.abs(diff) > Math.PI) {
+            goTheOtherWay = true
+        }
+
+        if (Math.abs(diff) < 0.1) {
+            this.angle = this.targetAngle;
+        } else {
+            let steering = 0.02
+            if (!goTheOtherWay) {
+                if (diff < 0) {
+                    this.angle += steering;
+                } else if (diff > 0) {
+                    this.angle -= steering;
+                }
+            } else {
+                if (diff > 0) {
+                    this.angle += steering;
+                } else if (diff < 0) {
+                    this.angle -= steering;
+                }
+            }
+        }
+
+
+        if (this.steeringAngle !== 0) {
+            this.angle += (this.speed / this.wheelbase) * Math.tan(this.steeringAngle) * deltaTime;
+        }
+
+        if (this.steeringAngle < -this.maxSteeringAngle) this.steeringAngle = -this.maxSteeringAngle;
+        if (this.steeringAngle > this.maxSteeringAngle) this.steeringAngle = this.maxSteeringAngle;
+
+        this.dx = Math.cos(this.angle) * this.speed;
+        this.dy = Math.sin(this.angle) * this.speed;
+        this.x += this.dx * deltaTime;
+        this.y += this.dy * deltaTime;
+    }
+
+    gtaSteering(deltaTime, keyboard) {
         if (keyboard !== null && keyboard !== undefined) {
             if (keyboard.isKeyDown(Buttons.LEFT)) {
                 this.turnLeft(deltaTime);
@@ -66,8 +195,8 @@ class Vehicle extends GameObject {
             if (keyboard.isKeyDown(Buttons.DOWN)) {
                 this.decelerate(deltaTime)
             }
-
         }
+
         const friction = 0.8; // keep 90% of speed per second
         this.speed *= Math.pow(friction, deltaTime);
 
@@ -82,6 +211,14 @@ class Vehicle extends GameObject {
         this.dy = Math.sin(this.angle) * this.speed;
         this.x += this.dx * deltaTime;
         this.y += this.dy * deltaTime;
+    }
+
+    update(deltaTime, keyboard) {
+        if (this.easySteering) {
+            this.easySteer(deltaTime, keyboard)
+        } else {
+            this.gtaSteering(deltaTime, keyboard)
+        }
     }
 
     draw(ctx) {
